c05cbef03367d7825fe2eb4892a57ab3a3b0099d
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2023-01-08 16:27:22 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,9 +3,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ClawSubsystem;
 
 public class RobotContainer {
     private final DriveSubsystem driveSubsystem;
+    private final ClawSubsystem clawSubsystem;
 
     private static XboxController driver;
     private static XboxController operator;
@@ -14,6 +16,11 @@ public class RobotContainer {
         //DRIVE
         driveSubsystem = new DriveSubsystem();
         CommandScheduler.getInstance().setDefaultCommand(driveSubsystem, new DefaultDriveCommand(driveSubsystem));
+
+        //CLAW
+        clawSubsystem = new ClawSubsystem();
+        CommandScheduler.getInstance().setDefaultCommand(clawSubsystem, new DefaultClawCommand(clawSubsystem));
+
 
         driver = new XboxController(0);
         operator = new XboxController(1);
