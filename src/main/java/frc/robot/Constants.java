b840f7df875c174df8e0e86c82279b4b316b1261
Matthew <matthewlee031205@gmail.com>
2023-01-10 04:30:45 -0500

    Inverted motorgroup and made slowmode a toggle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
 
 
     public static final double TRIGGER_DEADZONE = 0.2;
-    public static final double MOTOR_SPEED_NORMAL = 0.8;
+    public static final double MOTOR_SPEED_NORMAL = 0.7;
     public static final double MOTOR_SPEED_REDUCED = 0.35;
     public static final double ELEVATOR_SPEED_REDUCED = 0.25;
 }
